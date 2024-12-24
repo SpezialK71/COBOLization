15841b71c070a4ff441de3bb10538004e90cde6d
Kurt <130383415+SpezialK71@users.noreply.github.com>
2024-12-24 15:53:00 -0600

    COBOL xMas 2024

---
+++ MicroFocus_/COBOL Fundamentals/working-storage section.cbl
@@ -0,0 +1,23 @@
+       working-storage section.
+       01 employee.
+           05 employee-number         pic 9(5).
+           05 salary                  pic 9(5)V9(2).
+	       05 #name.
+	           10 first-name          pic X(10).
+	           10 last-name           pic X(10).
+		
+       01 salary-format               pic $(3),$(3).$(2) typedef.
+
+       procedure division.
+           move "000015000000Kurt    Rosengreen" to employee
+
+           declare formatted-salary as salary-format
+           move salary to formatted-salary
+
+           display "Employee Number: " employee-number
+           display "Salary: " formatted-salary
+           display "First Name: " first-name
+           display "Last Name: " last-name
+
+           stop run
+           .
